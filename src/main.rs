929434731e2a08e6b1e58e16a975bea25810d0fd
uditt lamba <uditt@suretyseven.com>
2025-04-08 23:43:35 +0530

    test app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use iced::widget::{Column, button, column, text};
+
+#[derive(Default)]
+struct Counter {
+    value: i64,
+}
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    Increment,
+    Decrement,
+}
+
+impl Counter {
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Increment => {
+                self.value += 1;
+            }
+            Message::Decrement => {
+                self.value -= 1;
+            }
+        }
+    }
+    fn view(&self) -> Column<Message> {
+        column![
+            button("+").on_press(Message::Increment),
+            text(self.value),
+            button("-").on_press(Message::Decrement),
+        ]
+    }
+}
+
+pub fn main() -> iced::Result {
+    iced::run("A cool counter", Counter::update, Counter::view)
 }
